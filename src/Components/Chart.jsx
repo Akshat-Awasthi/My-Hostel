67c93bd35728a234a210f44baab39d7864543806
Akshat Awasthi <aakshat444@gmail.com>
2024-03-30 00:41:46 +0530

    added today's menu in dash

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Chart = () => {
   return (
     <div>
-        
+        <Bar 
+                    data={{
+                        labels:Feedback.map((data)=>data.label),
+                    datasets: [
+                        {
+                            label:"No. of Students",
+                            data: Feedback.map((data)=>data.Value),
+                            backgroundColor:[
+                                "rgb(0,128,0)",
+                                "rgb(220,20,60)",
+                            ],
+                            borderRadius:5,
+                        },
+                    ]}} />
     </div>
   )
 }
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import { Chart as ChartJS, defaults } from "chart.js/auto";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
 import { PiHandWavingFill } from "react-icons/pi";
+import { Link } from 'react-router-dom';
 
 
 import Feedback from "./lib/const/FeedbackData.json"
 import WeekFeedback from "./lib/const/WeekFeedback.json"
+import TodayMenu from "./lib/const/TodayMenu.json"
 
 defaults.maintainAspectRatio = false;
 defaults.responsive = true;
 
 defaults.plugins.title.display = true;
 defaults.plugins.title.align = "start";
-defaults.plugins.title.font.size = 20;
+defaults.plugins.title.font.size = 16;
 defaults.plugins.title.color = "black";
 
 const Dashboard = () => {
+  const colors = ['bg-purple-200', 'bg-[#ACC3FD]', 'bg-[#BAE5F5]', 'bg-[#CCEFBF]'];
   return (
-    <div className='flex flex-row h-[1000px]'>
+    <div className='flex flex-row h-[100vh] overflow-y-auto'>
         <div className='flex flex-col w-3/4'>
-            <div className='flex flex-row h-40'>
-                <PiHandWavingFill />
-                <h1>Welcome, Akshat!</h1>
+            <div className='flex flex-row pl-3 pb-16 text-xl'>
+                <div className='text-xl pt-1'><PiHandWavingFill /></div>
+                <h1 className='pl-1'>Welcome, Akshat!</h1>
             </div>
             <div className='flex flex-col pl-3'>
             <div className='h-[45vh] '>
@@ -56,29 +59,33 @@ const Dashboard = () => {
                 />
             </div>
             <div className='flex flex-row'>
-                <div className='w-1/2 h-[40vh] m-2 bg-slate-100'>
-                    <Bar 
-                    data={{
-                        labels:Feedback.map((data)=>data.label),
-                    datasets: [
-                        {
-                            label:"No. of Students",
-                            data: Feedback.map((data)=>data.Value),
-                            backgroundColor:[
-                                "rgb(0,128,0)",
-                                "rgb(220,20,60)",
-                            ],
-                            borderRadius:5,
-                        },
-                    ]}} />
+            <div className='flex flex-col h-[40vh] m-2 bg-slate-100'>
+                <div className='flex flex-row mb-2 border-b-[1px] border-gray-300'>
+                <h2 className='mb-2 font-bold flex-1'>Today's Menu</h2>
+                <Link to='/Menu' className='mr-6'>View all</Link>
                 </div>
-                <div className='w-1/2 m-2'>chart 3</div>
+                <div className='flex'>
+                    {
+                        TodayMenu.map((item, index) => (
+                            <div key={item.time} className={`mr-6 w-48 h-52 p-6 pl-7 rounded-md ${colors[index % colors.length]}`}>
+                                <ul className='flex flex-col gap-3'>
+                                    <li className='font-semibold'>{item.time}</li>
+                                    <li className='list-disc'>{item.item1}</li>
+                                    <li className='list-disc'>{item.item2}</li>
+                                    <li className='list-disc'>{item.item3}</li>
+                                </ul>
+                            </div>
+                        ))
+                    }
+                </div>
+
+            </div>       
             </div>
-            <div className='h-72 mt-2 bg-blue-700'></div>
+            <div className='h-[100vh] mt-2'></div>
         </div>
         </div>
         
-        <div>right side</div>
+        <div className='h-[100vh] w-full ml-2 bg-blue-950'>right side</div>
         
     </div>
   )

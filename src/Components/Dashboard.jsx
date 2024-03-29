ca43587f62bc0c5a3db9c4af0e0800b09c4013c3
Akshat Awasthi <aakshat444@gmail.com>
2024-03-29 13:42:58 +0530

    added Charts

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import { Chart as ChartJS, defaults } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import { PiHandWavingFill } from "react-icons/pi";
+
+
+import Feedback from "./lib/const/FeedbackData.json"
+import WeekFeedback from "./lib/const/WeekFeedback.json"
+
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.font.size = 20;
+defaults.plugins.title.color = "black";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-row h-[1000px]'>
+        <div className='flex flex-col w-3/4'>
+            <div className='flex flex-row h-40'>
+                <PiHandWavingFill />
+                <h1>Welcome, Akshat!</h1>
+            </div>
+            <div className='flex flex-col pl-3'>
+            <div className='h-[45vh] '>
+                <Line 
+                data={{
+                    labels:WeekFeedback.map((data)=> data.label),
+                    datasets:[
+                        {label: "Positive",
+                        data: WeekFeedback.map((data) => data.Positive),
+                        backgroundColor: "#064FF0", 
+                        borderColor: "#064FF0",
+                    },
+                        {label: "Negative",
+                        data: WeekFeedback.map((data) => data.Negative),
+                        backgroundColor: "#FF3030",
+                        borderColor: "#FF3030",
+                    },
+                    ]
+                }}
+                options={{
+                    elements:{
+                        line:{
+                            tension:0.4
+                        },
+                    },
+                    plugins: {
+                        title: {
+                        text: "Weekly Feedback",
+                        },
+                    },
+                }}
+                />
+            </div>
+            <div className='flex flex-row'>
+                <div className='w-1/2 h-[40vh] m-2 bg-slate-100'>
+                    <Bar 
+                    data={{
+                        labels:Feedback.map((data)=>data.label),
+                    datasets: [
+                        {
+                            label:"No. of Students",
+                            data: Feedback.map((data)=>data.Value),
+                            backgroundColor:[
+                                "rgb(0,128,0)",
+                                "rgb(220,20,60)",
+                            ],
+                            borderRadius:5,
+                        },
+                    ]}} />
+                </div>
+                <div className='w-1/2 m-2'>chart 3</div>
+            </div>
+            <div className='h-72 mt-2 bg-blue-700'></div>
+        </div>
+        </div>
+        
+        <div>right side</div>
+        
+    </div>
   )
 }
 
--- src/Components/shared/Layout.jsx
+++ src/Components/shared/Layout.jsx
@@ -15,9 +15,9 @@ const Layout = () => {
         <div className='flex flex-row items-center h-16 w-[1190px] bg-neutral-100 border-y-2 border-blue-950'>
               <div className='flex-1 flex justify-center items-center text-2xl'>Hostel Name</div>
               <div className='mr-10 flex flex-row gap-5 text-2xl active:neutral-400 '>
-                <Link className='text-black'><MdNotificationsActive/></Link>
-                <Link className='text-black'><IoAlertCircle/></Link>
-                <Link to='/profile' className='text-black'><CgProfile /></Link>
+                <Link className='text-black hover:text-stone-400'><MdNotificationsActive/></Link>
+                <Link className='text-black hover:text-stone-400'><IoAlertCircle/></Link>
+                <Link to='/profile' className='text-black hover:text-stone-400'><CgProfile /></Link>
               </div>
             </div>
             <div>{<Outlet />}</div>

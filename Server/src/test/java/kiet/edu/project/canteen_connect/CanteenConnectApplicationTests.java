5ed7139d31f1f089bfa3c63a64635b476d9f4eb1
Abhinav Patel <116996708+abhinav-patel@users.noreply.github.com>
2024-12-14 13:38:27 +0530

    Updated CanteenConnectApplicationTests.java

--- Server/src/test/java/kiet/edu/project/canteen_connect/CanteenConnectApplicationTests.java
+++ Server/src/test/java/kiet/edu/project/canteen_connect/CanteenConnectApplicationTests.java
@@ -1,14 +1,16 @@
 package kiet.edu.project.canteen_connect;
 
+// Importing necessary classes for testing
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Annotation to specify that this is a Spring Boot Test class
 @SpringBootTest
 class CanteenConnectApplicationTests {
 
-	@Test
-	void contextLoads() {
-		
-	}
-
+    // Test method to verify if the Spring application context loads properly
+    @Test
+    void contextLoads() {
+        // This method is used to test whether the application context initializes without issues
+    }
 }
